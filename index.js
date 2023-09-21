baa13de451087103f865a76c79654d8db0cef400
Mihkelhain <114060635+Mihkelhain@users.noreply.github.com>
2023-09-21 12:27:05 +0300

    Get method in user

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const swaggerDocument = require('./docs/swagger.json')
 
 app.use('/docs', swaggerui.serve, swaggerui.setup(swaggerDocument))
 
+app.get("/user",(req, res) => {
+    res.send([
+        {id:1,name:'Bob',password:'1234',group:'GALpe20'},
+        {id:2,name:'jebediah',password:'0987654321',group:'MELpe22'},
+        {id:3,name:'martin',password:'b_phi<ibh',group:'TARpe21'},
+        {id:4,name:'Hannes',password:'säsn',group:'TARpe21'}
+    ])
+}
+)
+
 app.listen(port, () => {
-    console.log(`API up at: Hppt://Localhost:${port}`)
+    console.log(`API up at: Http://Localhost:${port}`)
 })
