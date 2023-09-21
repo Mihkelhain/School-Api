c3facf074166a98809b8e38a1999db6b399ba968
Opilane TTHK <opilane@tthk.ee>
2023-09-21 10:39:05 +0300

    [fixes #186040081]  Add swagger-ui and intial documentation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('express')()
 const port = 8080
+const swaggerui = require('swagger-ui-express')
+const swaggerDocument = require('./Docs/swagger.json')
+
+app.use('/Docs'. swaggerui.Serve, swaggerui.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: Hppt://Localhost:${port}`)
